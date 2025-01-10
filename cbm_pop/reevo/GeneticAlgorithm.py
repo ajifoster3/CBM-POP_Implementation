e1b65b5d7abfd6abda980ed1ca3bd3d0044944c5
Ash <ajifoster3@gmail.com>
2025-01-10 14:01:47 +0000

    MOH-2 Implement Mutation

--- cbm_pop/reevo/GeneticAlgorithm.py
+++ cbm_pop/reevo/GeneticAlgorithm.py
@@ -2,11 +2,12 @@ import random
 from cbm_pop.Problem import Problem
 
 class GeneticAlgorithm:
-    def __init__(self, population_size, number_of_tasks, number_of_agents, crossover_function_code):
+    def __init__(self, population_size, number_of_tasks, number_of_agents, crossover_function_code, mutation_function_code):
         self.population_size = population_size
         self.number_of_tasks = number_of_tasks
         self.number_of_agents = number_of_agents
         self.crossover_function = self._load_crossover_function(crossover_function_code)
+        self.mutation_function = self._load_mutation_function(mutation_function_code)
 
     def _load_crossover_function(self, code):
         """
@@ -15,6 +16,14 @@ class GeneticAlgorithm:
         local_namespace = {}
         exec(code, globals(), local_namespace)
         return local_namespace['ga_crossover_v2']
+
+    def _load_mutation_function(self, code):
+        """
+        Dynamically loads a Python function from a code string.
+        """
+        local_namespace = {}
+        exec(code, globals(), local_namespace)
+        return local_namespace['ga_mutation_v2']
 
     def generate_initial_population(self):
         population = []
@@ -56,3 +65,6 @@ class GeneticAlgorithm:
 
     def perform_crossover(self, parent1, parent2, cost_matrix):
         return self.crossover_function(parent1, parent2, cost_matrix)
+
+    def perform_mutation(self, parent, cost_matrix):
+        return self.mutation_function(parent, cost_matrix)
