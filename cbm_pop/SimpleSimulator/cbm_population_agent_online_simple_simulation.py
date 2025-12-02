403a6ddad6f67fe6f1043fa03a99ce611ae8c886
Ash <ajifoster3@gmail.com>
2025-12-02 15:11:49 +0000

    Simple simulator, return to base on failure

--- cbm_pop/SimpleSimulator/cbm_population_agent_online_simple_simulation.py
+++ cbm_pop/SimpleSimulator/cbm_population_agent_online_simple_simulation.py
@@ -1640,12 +1640,8 @@ class CBMPopulationAgentOnlineSimpleSimulation(Node):
                 goal_pose = SimplePosition()
 
                 goal_pose.robot_id = self.agent_ID
-                goal_pose.x_position = self.robot_poses[self.agent_ID][0] if self.robot_poses[
-                                                                                 self.agent_ID] is not None else \
-                                                                            self.initial_robot_poses[self.agent_ID][0]
-                goal_pose.y_position = self.robot_poses[self.agent_ID][1] if self.robot_poses[
-                                                                                 self.agent_ID] is not None else \
-                                                                            self.initial_robot_poses[self.agent_ID][1]
+                goal_pose.x_position = self.initial_robot_poses[self.agent_ID][0]
+                goal_pose.y_position = self.initial_robot_poses[self.agent_ID][1]
                 self.goal_pose_publisher.publish(goal_pose)
 
                 self.current_task = None
